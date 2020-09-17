617b249e21c9b36e87d133d00cbd574197a3cf7e
abdelouahed <doonabdo98@gmail.com>
2020-09-17 21:21:08 +0000

    add STU S6 Options

---
+++ src/pages/licence-fondamental/STU/S6/BassinsSedimentaireEtGeoressources.vue
@@ -0,0 +1,37 @@
+<template>
+	<ContentDisplayer :smodules="smodules" :slugs="slugs" :path="path">
+	</ContentDisplayer>
+</template>
+
+<script defer>
+export default {
+	metaInfo: () => ({
+		title:
+			"Licence Fondamental SMP-S6-Bassins-Sedimentaire-et-Georessources",
+		meta: [
+			{
+				name: "description",
+				content:
+					"Licence Fondamental SMP-S6-Bassins-Sedimentaire-et-Georessources courses",
+			},
+		],
+	}),
+	// Data =================
+	data: () => ({
+		smodules: [
+			"Bassins sedimentaires",
+			"Series sedimentaires et exploration petroliere",
+			"Geologie De La Matiere Organique ET des Phosphates",
+			"Geologie des hydrocarbures - systemes petroliers",
+		],
+		slugs: [
+			"Bassins_sedimentaires",
+			"Series_sedimentaires_et_exploration_petroliere",
+			"Geologie_De_LA_MATIERE_ORGANIQUE_ET_des_Phosphates",
+			"Geologie_des_hydrocarbures_systemes_petroliers",
+		],
+		path:
+			"/Licence Fondamental/S6/SMP/Bassins Sedimentaire et Georessources",
+	}),
+};
+</script>
---
+++ src/pages/licence-fondamental/STU/S6/GeologieAppliquee.vue
@@ -0,0 +1,35 @@
+<template>
+	<ContentDisplayer :smodules="smodules" :slugs="slugs" :path="path">
+	</ContentDisplayer>
+</template>
+
+<script defer>
+export default {
+	metaInfo: () => ({
+		title: "Licence Fondamental SMP-S6-Geologie-Appliquee",
+		meta: [
+			{
+				name: "description",
+				content:
+					"Licence Fondamental SMP-S6-Geologie-Appliquee courses",
+			},
+		],
+	}),
+	// Data =================
+	data: () => ({
+		smodules: [
+			"Bassins sedimentaires",
+			"Series sedimentaires et exploration petroliere",
+			"Geologie De LA MATIERE ORGANIQUE ET des Phosphates",
+			"Geologie des hydrocarbures - systemes petroliers",
+		],
+		slugs: [
+			"Metallogenie_typologique",
+			"Cycle_de_lexploration_minerale",
+			"Ecoulements_et_crues_dans_les_bassins_versants",
+			"Hydrogeologie_et_Hydrochimie",
+		],
+		path: "/Licence Fondamental/S6/SMP/Geologie Appliquee",
+	}),
+};
+</script>
